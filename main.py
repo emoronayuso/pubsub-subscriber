7450fb665a8d7de70d11e525d904b7ffadeaa985
Enrique Morón Ayuso <emoronayuso@gmail.com>
2022-07-03 23:09:17 +0200

    feat: Add first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from google.cloud import pubsub_v1
+
+SUBSCRIPTION_PATH = 'projects/pruebas-pubsub-systerminal/subscriptions/topic_cf-subscription'
+subscriber = pubsub_v1.SubscriberClient()
+
+def subscriber_cf(message: subscriber.message.Message) -> None:
+
+    ##############################
+    # read data from topic!
+
+#    subscriber = pubsub_v1.SubscriberClient()
+
+    print(f"Received {message}.")
+    message.ack()
+
+#    blob = bucket.get_blob(event['name'])
+#    data_json_str = blob.download_as_string()
+#    data_json = json.loads(data_json_str)
+
+#    sensor_name = data_json['sensorName']
+#    temperature = data_json['temperature']
+#    humidity = data_json['humidity']
+
+
+    ###############################
+    # move the data to Firestore!
+
+
