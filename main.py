bb9df759fe90e1ce7c57c9aa0590c2796c570a62
Enrique Morón Ayuso <emoronayuso@gmail.com>
2022-07-08 12:39:43 +0200

    fix: data sensors to firestore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import base64
 from google.cloud import pubsub_v1
 from google.cloud import firestore
 from datetime import datetime
@@ -7,10 +8,25 @@ from datetime import datetime
 SUBSCRIPTION_PATH = 'projects/pruebas-pubsub-systerminal/subscriptions/topic_cf-subscription'
 
 def subscriber_cf(event, context):
-    """Triggered from a message on a Cloud Pub/Sub topic.
+    """Background Cloud Function to be triggered by Pub/Sub.
     Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
+         event (dict):  The dictionary with data specific to this type of
+                        event. The `@type` field maps to
+                         `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
+                        The `data` field maps to the PubsubMessage data
+                        in a base64-encoded string. The `attributes` field maps
+                        to the PubsubMessage attributes if any is present.
+         context (google.cloud.functions.Context): Metadata of triggering event
+                        including `event_id` which maps to the PubsubMessage
+                        messageId, `timestamp` which maps to the PubsubMessage
+                        publishTime, `event_type` which maps to
+                        `google.pubsub.topic.publish`, and `resource` which is
+                        a dictionary that describes the service API endpoint
+                        pubsub.googleapis.com, the triggering topic's name, and
+                        the triggering event type
+                        `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
+    Returns:
+        None. The output is written to Cloud Logging.
     """
 
     print("This Function was triggered by messageId {} published at {}".format(context.event_id, context.timestamp))
@@ -18,12 +34,15 @@ def subscriber_cf(event, context):
     ##############################
     # read data from topic!
 
-    print(event['data']['readings'])
+    #print(event['data'][0]['readings'][0])
 
     if 'data' in event:
-        sensor_id = event['data']['readings']['sensorId']
-        temperature = event['data']['readings']['temperature']
-        humidity = event['data']['readings']['humidity']
+        sensor_id = base64.b64decode(event['data']['readings']['sensorId']).decode('utf-8')
+        #sensor_id = event['data']['readings']['sensorId']
+        #temperature = event['data']['readings']['temperature']
+        temperature = base64.b64decode(event['data']['readings']['temperature']).decode('utf-8')
+        humidity = base64.b64decode(event['data']['readings']['humidity']).decode('utf-8')
+        #humidity = event['data']['readings']['humidity']
     else:
         sensor_name = 0
         temperature = 0
@@ -40,19 +59,19 @@ def subscriber_cf(event, context):
     db = firestore.Client()
     doc_ref = db.collection(u'data').document(u'sensors')
 
-    data = json.dumps(event['data']['readings'])
+    #data = json.dumps(event['data']['readings'])
 
     #data['sensorId'] = sensor_id
     #data['temperature'] = temperature
     #data['humidity'] = humidity
-    data['datetime'] = datetime.now() 
+    #data['datetime'] = datetime.now() 
 
     doc_ref.set(data)
 
-#    doc_ref.set({
-#        u'sensorId': sensor_id,
-#        u'temperature': temperature,
-#        u'humidity': humidity,
-#        u'datetime': datetime.now()
-#    })
+    doc_ref.set({
+        u'sensorId': sensor_id,
+        u'temperature': temperature,
+        u'humidity': humidity,
+        u'datetime': datetime.now()
+    })
 
